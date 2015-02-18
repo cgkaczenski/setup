95d8e08fd5a4ed2eb0069cd9db4f02a6d2f0c7da
cgkaczenski <kzpulaski@gmail.com>
2015-02-18 17:58:51 -0600

    test setup for rabbitmq

---
+++ rabbit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Setup for rabbitmq server/message bus on Ubuntu 14.04 EC2 instance
+
+#install rabbitmq-server 3.2.1
+#From Rabbitmq Essentials:
+#first attempt to install fails and generates list of Erlang dependencies
+wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.1/rabbitmq-server_3.2.1-1_all.deb
+sudo dpkg -i rabbitmq-server_3.2.1-1_all.deb
+sudo apt-get -f --force-yes --yes install
+
+#install management plugin
+sudo rabbitmq-plugins enable rabbitmq_managment
+sudo services rabbitmq-server restart
+

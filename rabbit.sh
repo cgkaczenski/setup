139c8bde07957092fff1c1dfd594738b393eaa61
cgkaczenski <kzpulaski@gmail.com>
2015-02-26 05:08:49 -0600

    new script

--- rabbit.sh
+++ rabbit.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
-# Setup for rabbitmq server/message bus on Ubuntu 14.04 EC2 instance
+#!/bin/sh
+cat <<EOF
+EOF > /etc/apt/sources.list.d/rabbitmq.list
+deb http://www.rabbitmq.com/debian/ testing main
+EOF
 
-#install rabbitmq-server 3.2.1
-#From Rabbitmq Essentials:
-#first attempt to install fails and generates list of Erlang dependencies
-wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.1/rabbitmq-server_3.2.1-1_all.deb
-sudo dpkg -i rabbitmq-server_3.2.1-1_all.deb
-sudo apt-get -f --force-yes --yes install
+curl http://www.rabbitmq.com/rabbitmq-signing-key-public.asc -o /tmp/rabbitmq-signing-key-public.asc
+apt-key add /tmp/rabbitmq-signing-key-public.asc
+rm /tmp/rabbitmq-signing-key-public.asc
 
-#install management plugin
-sudo rabbitmq-plugins enable rabbitmq_managment
-sudo service rabbitmq-server restart
+apt-get -qy update
+apt-get -qy install rabbitmq-server
+EOF
+
 
